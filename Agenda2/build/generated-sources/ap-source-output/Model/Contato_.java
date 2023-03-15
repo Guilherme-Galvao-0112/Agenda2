f515e90457189d11e284c60f0a037440d3bc92a1
Guilherme <guilhermegalvao925@gmail.com>
2023-03-14 20:18:18 -0400

    Adição do banco de dados, erro ao rodar os testes

---
+++ Agenda2/build/generated-sources/ap-source-output/Model/Contato_.java
@@ -0,0 +1,14 @@
+package Model;
+
+import javax.annotation.processing.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2023-03-14T20:15:19", comments="EclipseLink-2.7.10.v20211216-rNA")
+@StaticMetamodel(Contato.class)
+public class Contato_ { 
+
+    public static volatile SingularAttribute<Contato, String> nome;
+    public static volatile SingularAttribute<Contato, Integer> id;
+
+}
--- Agenda2/src/banco/BancoDAO.java
+++ Agenda2/src/banco/BancoDAO.java
@@ -4,10 +4,31 @@
  */
 package banco;
 
-/**
- *
- * @author laboratorio
- */
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+
 public class BancoDAO {
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("Agenda2PU");
+    EntityManager em = emf.createEntityManager();
+    EntityTransaction etx = em.getTransaction();
+    
+    public void insertOrUpdate(Object o){
+        etx.begin();
+        em.merge(o);
+        etx.commit();
+    }
+    public void remove(Object o){
+        etx.begin();
+        em.merge(o);
+        etx.commit();
+    }
+     public List list(String nomeClasse){
+       return em.createQuery("select o from "+nomeClasse+" o ").getResultList();
+       
+   }
     
 }
